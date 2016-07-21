4112d12cc1ebb574d471b168effee28ea2ee7428
Raphaël <raphael.godart@outlook.com>
2016-07-21 16:32:35 +0200

    Added assembly version

---
+++ Digimezzo.WPFControls/SharedAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Digimezzo")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2015 - 2016")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
