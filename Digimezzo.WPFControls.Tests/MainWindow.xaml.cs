581eb65d0f4d17e98fa0e03252a251ebe7859703
Raphaël <raphael.godart@outlook.com>
2017-03-05 20:28:39 +0100

    Revert "Added first draft of Android ripple effect"
    This reverts commit 4d0a02de4b93d496dcaa79e2fe2682e45a1a647e.

--- Digimezzo.WPFControls.Tests/MainWindow.xaml.cs
+++ Digimezzo.WPFControls.Tests/MainWindow.xaml.cs
@@ -103,11 +103,5 @@ namespace Digimezzo.WPFControls.Tests
             var win = new PivotTest();
             win.Show();
         }
-
-        private void RippleTestButton_Click(object sender, RoutedEventArgs e)
-        {
-            var win = new RippleTest();
-            win.Show();
-        }
     }
 }
