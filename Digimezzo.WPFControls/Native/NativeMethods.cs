fe71d535fd495df7191ad8abed29714e07050414
PeoLeser <peoleser@gmail.com>
2017-04-19 19:09:00 +0900

    Minor change

--- Digimezzo.WPFControls/Native/NativeMethods.cs
+++ Digimezzo.WPFControls/Native/NativeMethods.cs
@@ -10,11 +10,11 @@ namespace Digimezzo.WPFControls.Native
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
-        [DllImport("user32", EntryPoint = "GetMonitorInfoW", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        [DllImport("user32.dll", EntryPoint = "GetMonitorInfoW", ExactSpelling = true, CharSet = CharSet.Unicode)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool GetMonitorInfo(IntPtr hMonitor, [Out] MONITORINFO lpmi);
 
-        [DllImport("user32")]
+        [DllImport("user32.dll")]
         internal static extern IntPtr MonitorFromWindow(IntPtr handle, uint flags);
 
         [DllImport("user32.dll")]
