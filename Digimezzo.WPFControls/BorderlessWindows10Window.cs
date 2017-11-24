9df95486f8363d06f2bfe0ab9dd4444d82370ea2
Raphaël <raphael.godart@outlook.com>
2017-11-24 08:25:05 +0100

    Added property ApplyDefaultButtonColors which allows overriding the default button colors

--- Digimezzo.WPFControls/BorderlessWindows10Window.cs
+++ Digimezzo.WPFControls/BorderlessWindows10Window.cs
@@ -6,6 +6,15 @@ namespace Digimezzo.WPFControls
 {
     public abstract class BorderlessWindows10Window : BorderlessWindowBase
     {
+        public bool ApplyDefaultButtonColors
+        {
+            get { return (bool)GetValue(ApplyDefaultButtonColorsProperty); }
+            set { SetValue(ApplyDefaultButtonColorsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ApplyDefaultButtonColorsProperty =
+            DependencyProperty.Register(nameof(ApplyDefaultButtonColors), typeof(bool), typeof(BorderlessWindowBase), new PropertyMetadata(true));
+
         static BorderlessWindows10Window()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(BorderlessWindows10Window), new FrameworkPropertyMetadata(typeof(BorderlessWindows10Window)));
@@ -14,19 +23,23 @@ namespace Digimezzo.WPFControls
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            this.TitleBarHeight = 29;
-            this.MinMaxBackground = Brushes.Transparent;
-            this.MinMaxHoveredBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E5E5E5"));
-            this.MinMaxPressedBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#CACACB"));
-            this.MinMaxForeground = Brushes.Black;
-            this.MinMaxHoveredForeground = Brushes.Black;
-            this.MinMaxPressedForeground = Brushes.Black;
-            this.CloseBackground = Brushes.Transparent;
-            this.CloseHoveredBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E81123"));
-            this.ClosePressedBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F1707A"));
-            this.CloseForeground = Brushes.Black;
-            this.CloseHoveredForeground = Brushes.White;
-            this.ClosePressedForeground = Brushes.White;
+
+            if (this.ApplyDefaultButtonColors)
+            {
+                this.TitleBarHeight = 29;
+                this.MinMaxBackground = Brushes.Transparent;
+                this.MinMaxHoveredBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E5E5E5"));
+                this.MinMaxPressedBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#CACACB"));
+                this.MinMaxForeground = Brushes.Black;
+                this.MinMaxHoveredForeground = Brushes.Black;
+                this.MinMaxPressedForeground = Brushes.Black;
+                this.CloseBackground = Brushes.Transparent;
+                this.CloseHoveredBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E81123"));
+                this.ClosePressedBackground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F1707A"));
+                this.CloseForeground = Brushes.Black;
+                this.CloseHoveredForeground = Brushes.White;
+                this.ClosePressedForeground = Brushes.White;
+            }
         }
     }
 }
