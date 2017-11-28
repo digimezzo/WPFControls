cc426025c72e463519548273dad69f259b32b978
Raphaël <raphael.godart@outlook.com>
2017-11-28 06:58:39 +0100

    Added flat material button

--- Digimezzo.WPFControls/MaterialButtons.cs
+++ Digimezzo.WPFControls/MaterialButtons.cs
@@ -4,7 +4,7 @@ using System.Windows.Media;
 
 namespace Digimezzo.WPFControls
 {
-    public class MaterialRaisedButton : Button
+    public class MaterialButtonBase : Button
     {
         public Brush Accent
         {
@@ -13,11 +13,27 @@ namespace Digimezzo.WPFControls
         }
 
         public static readonly DependencyProperty AccentProperty =
-            DependencyProperty.Register(nameof(Accent), typeof(Brush), typeof(MaterialRaisedButton), new PropertyMetadata(Brushes.Red));
+            DependencyProperty.Register(nameof(Accent), typeof(Brush), typeof(MaterialButtonBase), new PropertyMetadata(Brushes.Red));
 
+        static MaterialButtonBase()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MaterialButtonBase), new FrameworkPropertyMetadata(typeof(MaterialButtonBase)));
+        }
+    }
+
+    public class MaterialRaisedButton : MaterialButtonBase
+    {
         static MaterialRaisedButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MaterialRaisedButton), new FrameworkPropertyMetadata(typeof(MaterialRaisedButton)));
+        }
+    }
+
+    public class MaterialFlatButton : MaterialButtonBase
+    {
+        static MaterialFlatButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MaterialFlatButton), new FrameworkPropertyMetadata(typeof(MaterialFlatButton)));
         }
     }
 }
