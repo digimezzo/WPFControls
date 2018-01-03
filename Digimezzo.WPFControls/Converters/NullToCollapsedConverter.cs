15ea0ed67dcda99fb6291f20b4be70ca193336a8
Raphaël <raphael.godart@outlook.com>
2018-01-03 20:35:23 +0100

    Fixed issues in material buttons, added nulltocollapsedconverter

---
+++ Digimezzo.WPFControls/Converters/NullToCollapsedConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Digimezzo.WPFControls.Converters
+{
+    public class NullToCollapsedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
