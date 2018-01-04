09b2fc07b9e5fd227d675c1748fbb9acac83ac6b
Raphaël <raphael.godart@outlook.com>
2018-01-04 22:45:56 +0100

    Added InvertingNullToCollapsedConverter

---
+++ Digimezzo.WPFControls/Converters/InvertingNullToCollapsedConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Digimezzo.WPFControls.Converters
+{
+    public class InvertingNullToCollapsedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
